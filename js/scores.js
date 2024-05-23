8aae891187cc56e4139cc4aa31b9aa5ed41dfa72
mojtaba nabilu <mojjtaba00@gmail.com>
2024-05-24 00:37:06 +0330

    completed scores page

--- js/scores.js
+++ js/scores.js
@@ -1,8 +1,21 @@
-const userPoints = localStorage.getItem("high-scores") || [];
-const ol = document.querySelector("ol");
+const userPoints = JSON.parse(localStorage.getItem("high-scores"));
+userPoints.sort((a, b) => b.score - a.score);
 
-if (!JSON.parse(userPoints)) {
-    console.log("null");
+const ol = document.querySelector("ol");
+const noScoreAlert = document.querySelector("h4");
+
+const content = (ol.innerHTML = userPoints.map((item, index) => {
+  return `
+            <li>
+              <span>${index + 1}</span>
+              <p>${item.name}</p>
+              <span>${item.score}</span>
+            </li>
+          `;
+}));
+
+if (userPoints) {
+    ol.innerHTML = content.join("")
 } else {
-    console.log("array");
+  noScoreAlert.style.display = "block";
 }
